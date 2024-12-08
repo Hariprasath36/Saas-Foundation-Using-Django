185457290d82d1052279a7579e9707a4d079858e
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-08 13:34:28 +0530

    running 35:00

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import HttpResponse
+import pathlib
 
-
-
+this_dir = pathlib.Path(__file__).parent.resolve()
 def home_page_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Home Page</h1>")
+     my_title= "My Home Page"
+     my_context= {
+          "title": my_title
+     }
+     html_= """
+     <!DOCTYPE html>
+     <html>
+     <body>
+     <h1>{title} anything?</h1>
+     
+     </body>
+     </html>
+     """.format(**my_context)
+     return HttpResponse(html_)
+    
