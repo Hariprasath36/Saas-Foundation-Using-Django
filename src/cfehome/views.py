62d03991a179bd55ba498b78472af54400e5f17d
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-08 13:55:27 +0530

    running 43:00

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 import pathlib
+from django.http import HttpResponse
+
 
 this_dir = pathlib.Path(__file__).parent.resolve()
+
 def home_page_view(request, *args, **kwargs):
+    my_title= "My Page"
+    my_context= {
+         "title": my_title
+    }
+    html_template= "home.html"
+    return render(request,html_template,my_context)
+
+def old_home_page_view(request, *args, **kwargs):
      my_title= "My Home Page"
      my_context= {
           "title": my_title
