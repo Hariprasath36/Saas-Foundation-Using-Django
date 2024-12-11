a784a839d14610b4cbaf2d6f1f64653e14407345
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-11 13:05:35 +0530

    running 1:02:00

--- src/visits/models.py
+++ src/visits/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class PageVisit(models.Model):
+    # db -> table
+    # id -> hiddem-> primary key-> auto increment
+    path = models.TextField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
